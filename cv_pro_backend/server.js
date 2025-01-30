9d3fe0f45b676252a46343b1530caaa4499f52f8
Divviya <85920511+DivviyaC@users.noreply.github.com>
2025-01-30 13:33:31 +0530

    Update server.js

--- cv_pro_backend/server.js
+++ cv_pro_backend/server.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-// const openaiRoute = require("./routes/openaiRoute");
-// const supabaseRoute = require("./routes/supbaseRoute")
-// const cvroutes = require("./routes/cvroutes");
 const jdRoutes = require("./routes/jdroutes");
 const atsRoutes = require("./routes/atsroutes");
 require("dotenv").config();
@@ -14,9 +11,6 @@ app.use(express.json());
 app.use(cors());
 
 // Use the OpenAI route
-// app.use("/api/openai", openaiRoute);
-// app.use("/api/supabase", supabaseRoute);
-// app.use("/api", cvroutes);
 app.use("/api", jdRoutes);
 app.use("/api", atsRoutes);
 
